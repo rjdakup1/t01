9eb0b2b38ad9ec89881a99d02cf36c211d3e9fc2
rjdakup1 <rjdakup1@cougars.ccis.edu>
2014-10-26 00:16:25 -0500

    check main.cpp

--- t01q01/main.cpp
+++ t01q01/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <vector>
+#include "Cell.h"
+
 
 int main()
 {
     int n, r, c;
     std::cin >> n >> r >> c;
 
+    // Create constants to store the size of the maze
     const int ROW = n;
     const int COL = n;
+
+    // Create a container of unvisited cells
+    std::vector< Cell > UNVISITED;
+
+    // Create a stack of cell objects named path
+    std::vector< Cell > PATH;
+
+    // Create a container of visited cells
+    std::vector< Cell > VISITED;
+ 
 
     return 0;
 }
